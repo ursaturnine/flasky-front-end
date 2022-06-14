a2786e3965e6e378f62a88396e5f19c1888d3e37
Tyrah <tyrah96@gmail.com>
2022-06-14 10:27:31 -0700

    cat component practice

---
+++ src/components/Cat.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Cat = () => {
+  return (
+    <ul>
+      <li>
+        <h3>Mario!</h3>
+        <button>Pet Mario</button>
+      </li>
+    </ul>
+  );
+};
+
+export default Cat;
---
+++ src/components/CatList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Cat from "./Cat.js";
+
+const CatList = () => {
+  return (
+    <li>
+      <Cat />
+      <Cat />
+      <Cat />
+    </li>
+  );
+};
+
+export default CatList;
